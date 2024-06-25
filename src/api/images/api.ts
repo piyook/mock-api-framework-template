d8e868beafbf8813517f2cd7e0f48b8b04c24a5a
piyook <piyook@piyook.com>
2024-06-25 13:54:08 +0100

    chore(): add media info page to media routes

--- src/api/images/api.ts
+++ src/api/images/api.ts
@@ -6,6 +6,22 @@ import { http, HttpResponse } from 'msw';
 
 function handler(pathName: string) {
     return [
+        http.get(`/${pathName}`, ({ request }) => {
+            return HttpResponse.text(
+                `<body style="background-color: #383838; color:white">
+                <div style="text-align:center; padding:50px 0px 0px 0px">
+                <h4>Access images stored in the src/media/images folder using the format: <span style="color:red">api/images/{filename}</span></h4>
+                <h4>Example: api/images/placeholder.png</h4>
+                </div>
+                </body>
+                `,
+                {
+                    headers: {
+                        'Content-Type': 'text/html',
+                    },
+                },
+            );
+        }),
         http.get(`/${pathName}/:imageID`, ({ request }) => {
             const url = new URL(request.url);
             const params = url.pathname.split('/').pop();
--- src/api/videos/api.ts
+++ src/api/videos/api.ts
@@ -6,6 +6,22 @@ import { http, HttpResponse } from 'msw';
 
 function handler(pathName: string) {
     return [
+        http.get(`/${pathName}`, ({ request }) => {
+            return HttpResponse.text(
+                `<body style="background-color: #383838; color:white">
+                <div style="text-align:center; padding:50px 0px 0px 0px">
+                <h4>Access videos stored in the src/media/videos folder using the format: <span style="color:red">api/videos/{filename}</span></h4>
+                <h4>Example: api/videos/placeholder.mp4</h4>
+                </div>
+                </body>
+                `,
+                {
+                    headers: {
+                        'Content-Type': 'text/html',
+                    },
+                },
+            );
+        }),
         http.get(`/${pathName}/:videoID`, ({ request }) => {
             const url = new URL(request.url);
             const params = url.pathname.split('/').pop();
