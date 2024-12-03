8df90573c5d5dde73f1d9ff55b3ff9f3bbcb3d09
piyook <piyook@piyook.com>
2024-12-03 18:19:35 +0000

    chore(): add zod validation of env vars

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,17 @@ import { createServer } from '@mswjs/http-middleware';
 import * as seeders from './seeders/index.js';
 import getApiPaths from './utilities/file-scan.js';
 import serverPage from './utilities/server-page.js';
+import { env } from './utilities/env.js';
 
 const { apiHandlers, apiPaths } = await getApiPaths();
 
 const httpServer = createServer({}, ...apiHandlers, ...serverPage(apiPaths));
 
-httpServer.listen(process.env.SERVER_PORT);
+httpServer.listen(env.SERVER_PORT);
 
 // Execute dB seeder functions
 for (const seeder of Object.values(seeders)) {
     seeder();
 }
 
-console.log(`SERVER UP AND RUNNING ON LOCALHOST:${process.env.SERVER_PORT}`);
+console.log(`SERVER UP AND RUNNING ON LOCALHOST:${env.SERVER_PORT}`);
