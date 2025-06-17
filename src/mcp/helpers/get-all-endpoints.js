71a91e13a2298e98f451174494f970e939a043ab
piyook <piyook@piyook.com>
2025-06-17 14:38:05 +0100

    chore(): update readme

--- src/mcp/helpers/get-all-endpoints.js
+++ src/mcp/helpers/get-all-endpoints.js
@@ -1,14 +1,13 @@
 const getApiEndpoints = async (port) => {
-    let response;
-    try {
-        response = await fetch(`http://localhost:${port}`);
-        if (!response.ok) {
-            return 'Local mock API server is not running - please start the server first.';
-        }
-    }
-    catch {
-        return 'Local mock API server is not running - please start the server first.';
-    }
-    return response.text();
+	let response;
+	try {
+		response = await fetch(`http://localhost:${port}`);
+		if (!response.ok) {
+			return 'Local mock API server is not running - please start the server first.';
+		}
+	} catch {
+		return 'Local mock API server is not running - please start the server first.';
+	}
+	return response.text();
 };
 export { getApiEndpoints };
